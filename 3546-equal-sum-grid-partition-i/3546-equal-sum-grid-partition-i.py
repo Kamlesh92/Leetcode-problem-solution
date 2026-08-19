fb550abf0dc6d14197b3adae79a6db4e9c3fbf19
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-19 23:14:47 +0530

    Time: 258 ms (8.44%), Space: 45.7 MB (6.02%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.py
@@ -1,25 +1,31 @@
 class Solution:
-    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+    def canPartitionGrid(self, grid):
         m, n = len(grid), len(grid[0])
-        total = sum(sum(row) for row in grid)
-        
-        if total % 2:
-            return False
-        
-        target = total // 2
-        s = 0
-        
-        for i in range(m - 1):
-            s += sum(grid[i])
-            if s == target:
-                return True
-        
-        s = 0
-        
+
+        sumGrid = [[0] * n for _ in range(m)]
+
+        # first row
+        pref_sum = 0
+        for j in range(n):
+            pref_sum += grid[0][j]
+            sumGrid[0][j] = pref_sum
+
+        for i in range(1, m):
+            pref_sum = 0
+            for j in range(n):
+                pref_sum += grid[i][j]
+                sumGrid[i][j] = pref_sum + sumGrid[i - 1][j]
+
+        total_sum = sumGrid[m - 1][n - 1]
+
+        # column cuts
         for j in range(n - 1):
-            for i in range(m):
-                s += grid[i][j]
-            if s == target:
+            if sumGrid[m - 1][j] == total_sum - sumGrid[m - 1][j]:
                 return True
-        
+
+        # row cuts
+        for i in range(m - 1):
+            if sumGrid[i][n - 1] == total_sum - sumGrid[i][n - 1]:
+                return True
+
         return False
