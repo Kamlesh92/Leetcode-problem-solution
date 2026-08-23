13b819555a2c2a9d3b75a3507e2bcc53db1c1eb7
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-23 21:45:05 +0530

    Time: 1 ms (43%), Space: 12.4 MB (19.5%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,3 +1,19 @@
 class Solution:
-    def canBeEqual(self, s1: str, s2: str) -> bool:
-        return sorted(s1[::2]) == sorted(s2[::2]) and sorted(s1[1::2]) == sorted(s2[1::2])
+    def canBeEqual(self, s1, s2):
+        even1, odd1 = [], []
+        even2, odd2 = [], []
+        
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                even1.append(s1[i])
+                even2.append(s2[i])
+            else:
+                odd1.append(s1[i])
+                odd2.append(s2[i])
+        
+        even1.sort()
+        even2.sort()
+        odd1.sort()
+        odd2.sort()
+        
+        return even1 == even2 and odd1 == odd2
