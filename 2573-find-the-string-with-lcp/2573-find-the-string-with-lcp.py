0ab2c65d31e265e27250b07e2842fecb46523608
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-23 21:43:28 +0530

    Time: 488 ms (16.33%), Space: 41.6 MB (91.84%) - LeetHub

--- 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.py
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.py
@@ -1,75 +1,54 @@
-class Solution:
-    class DSU:
-        def __init__(self, n):
-            self.parent = list(range(n))
-            self.rank = [0] * n
-
-        def findPar(self, x):
-            if self.parent[x] != x:
-                self.parent[x] = self.findPar(self.parent[x])
-            return self.parent[x]
-
-        def unite(self, x, y):
-            px = self.findPar(x)
-            py = self.findPar(y)
-
-            if px == py:
-                return
-
-            if self.rank[px] < self.rank[py]:
-                self.parent[px] = py
-            elif self.rank[px] > self.rank[py]:
-                self.parent[py] = px
-            else:
-                self.parent[px] = py
-                self.rank[py] += 1
-
-    def compute(self, word, dp):
-        n = len(word)
-        for i in range(n - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if word[i] == word[j]:
-                    if i + 1 < n and j + 1 < n:
-                        dp[i][j] = 1 + dp[i + 1][j + 1]
-                    else:
-                        dp[i][j] = 1
-                else:
-                    dp[i][j] = 0
-
+class Solution(object):
     def findTheString(self, lcp):
+        def nextChar():
+            letter = ord('a') - 1
+            while True:
+                letter += 1
+                yield chr(letter)
+
         n = len(lcp)
-        dsu = self.DSU(n)
-
+        res = []
+        charGen = nextChar()
         for i in range(n):
-            if lcp[i][i] != n - i:
-                return ""
+            for j in range(i+1):
+                if len(res) != i:
+                    break
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                if lcp[i][j] > 0:
-                    dsu.unite(i, j)
+                if lcp[i][j]:
+                    if j < i:
+                        res.append(res[j])
+                    else:
+                        res.append( next(charGen) )
 
-        grp = [''] * n
-        word = ['?'] * n
-        c = ord('a')
+            if res and res[-1] > 'z':
+                return ''
 
-        for i in range(n):
-            p = dsu.findPar(i)
-            if grp[p] == '':
-                if c > ord('z'):
-                    return ""
-                grp[p] = chr(c)
-                c += 1
-            word[i] = grp[p]
+        def naiveCheck(i, j):
+            val = 0
+            iSize, jSize = n - i, n - j
+            for k in range(min(iSize, jSize)):
+                ik, jk = i + k, j + k
+                if max(ik, jk) < len(res):
+                    val += res[i+k] == res[j+k]
+            if lcp[i][j] != val:
+                return False
+            return True
+
+        def matrixCheck(i, j):
+            val = 0
+            # if out of the range of res, or the letters at i and j are the same, set to 1
+            if i >= len(res) or j >= len(res) or res[j] == res[i]:
+                val = 1
+                ni, nj = i + 1, j + 1
+                if max(ni, nj) < n:
+                    val +=lcp[ni][nj]
+            if val != lcp[i][j]:
+                return False
+            return True
 
         for i in range(n):
             for j in range(n):
-                if lcp[i][j] == 0 and word[i] == word[j]:
-                    return ""
-
-        dp = [[0] * n for _ in range(n)]
-        self.compute(word, dp)
-
-        if dp == lcp:
-            return "".join(word)
-        return ""
+                if not matrixCheck(i, j):
+                    return ''
+                    
+        return ''.join(res)
