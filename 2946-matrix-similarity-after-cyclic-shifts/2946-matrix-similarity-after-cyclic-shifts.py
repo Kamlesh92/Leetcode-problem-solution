334911852af01ea54e3ff61e119b2267380a10a4
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-23 21:42:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.76%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.py
@@ -1,16 +1,15 @@
 class Solution:
-    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
-        n = len(mat[0])
-        shift = k % n
-        if shift == 0:
-            return True
-            
-        for i, row in enumerate(mat):
+    def areSimilar(self, mat, k):
+        m, n = len(mat), len(mat[0])
+        k %= n
+
+        for i in range(m):
             for j in range(n):
                 if i % 2 == 0:
-                    if row[j] != row[(j + shift) % n]:
-                        return False
+                    new_col = (j + k) % n
                 else:
-                    if row[j] != row[(j + n - shift) % n]:
-                        return False
+                    new_col = (j - k) % n
+
+                if mat[i][j] != mat[i][new_col]:
+                    return False
         return True
