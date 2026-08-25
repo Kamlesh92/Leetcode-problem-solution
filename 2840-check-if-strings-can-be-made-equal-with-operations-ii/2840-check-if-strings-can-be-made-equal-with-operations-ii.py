6f649cdec474ae3981b87a8173a151eaae10e6c8
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-25 10:32:01 +0530

    Time: 267 ms (13.83%), Space: 16.9 MB (9.57%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,10 +1,28 @@
-class Solution:
-    def checkStrings(self, s1: str, s2: str) -> bool:
-        freq=[0]*52
-        n=len(s1)
-        for i in range(n):
-            iOdd=i&1
-            freq[iOdd*26+ord(s1[i])-97]+=1
-            freq[iOdd*26+ord(s2[i])-97]-=1
-        return all(x==0 for x in freq)
+class Solution(object):
+    def checkStrings(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+
+        d1_even={}
+        d1_odd={}
+
+        d2_even={}
+        d2_odd={}
+
+        for i in range(len(s1)):
+            if i %2==0:
+                d1_even[s1[i]]=d1_even.get(s1[i],0)+1
+            else:
+                d1_odd[s1[i]]=d1_odd.get(s1[i],0)+1
+            
+        for i in range(len(s2)):
+            if i %2==0:
+                d2_even[s2[i]]=d2_even.get(s2[i],0)+1
+            else:
+                d2_odd[s2[i]]=d2_odd.get(s2[i],0)+1
+        return d1_even == d2_even and d1_odd == d2_odd
+
         
