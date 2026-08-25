8bb023e83c8c60a580f6e8e04d7c65a310452d7d
Kamlesh <219505404+Kamlesh92@users.noreply.github.com>
2026-08-25 10:33:49 +0530

    Time: 132 ms (100%), Space: 28.6 MB (90.91%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.py
+++ 2751-robot-collisions/2751-robot-collisions.py
@@ -1,30 +1,29 @@
 class Solution:
-    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
-        n = len(positions)
-        robots = [[positions[ind], healths[ind], directions[ind], ind] for ind in range(n)]
-        robots.sort()
+    def survivedRobotsHealths(self, positions, healths, directions):
+        order = sorted(range(len(positions)), key=positions.__getitem__)
         stack = []
-
-        for robot in robots:
-            if robot[2] == "R" or not stack or stack[-1][2] == "L":
-                stack.append(robot)
-                continue
-
-            if robot[2] == "L":
-                add = True
-                while stack and stack[-1][2] == "R" and add:
-                    last_health = stack[-1][1]
-                    if robot[1] > last_health:
+        
+        for i in order:
+            if directions[i] == 'R':
+                stack.append(i)
+            else:
+                hi = healths[i]
+                while stack and hi:
+                    j = stack[-1]
+                    hj = healths[j]
+                    
+                    if hj < hi:
+                        hi -= 1
+                        healths[j] = 0
                         stack.pop()
-                        robot[1] -= 1
-                    elif robot[1] < last_health:
-                        stack[-1][1] -= 1
-                        add = False
+                    elif hj > hi:
+                        healths[j] = hj - 1
+                        hi = 0
                     else:
+                        healths[j] = 0
+                        hi = 0
                         stack.pop()
-                        add = False
-
-                if add:
-                    stack.append(robot)
-
-        return [robot[1] for robot in sorted(stack, key=lambda robot: robot[3])]
+                
+                healths[i] = hi
+        
+        return [h for h in healths if h]
